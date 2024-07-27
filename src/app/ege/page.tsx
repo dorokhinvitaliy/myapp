586ed2644c2f6c54d55309e098ce6aaed8b48c64
dorokhinvitaliy <dorokhin2006@bk.ru>
2024-07-27 12:14:44 +0300

    v

--- src/app/ege/page.tsx
+++ src/app/ege/page.tsx
@@ -32,6 +32,7 @@ import CustomTable, { CustomTableBody, CustomTableCell, CustomTableHeader, Custo
 import { CheckedIcon, CloseOutlineIcon } from "../ui/icons";
 
 export default function Page() {
+    const [loadError, updLoadError] = useState(false);
     const [loading, changeLoading] = useState("no");
     const [upRows, chUpRows] = useState(false);
     const [rows, updRows] = useState(null);
@@ -54,10 +55,28 @@ export default function Page() {
                     'Content-Type': 'application/json',
                 },
             }).then((resp) => {
+                updLoadError(false);
                 const rows_t = resp.data;
                 updRows(rows_t);
                 changeLoading("loaded");
-            });
+            }).catch(function (error) {
+                updLoadError(true);
+                if (error.response) {
+                  // The request was made and the server responded with a status code
+                  // that falls out of the range of 2xx
+                  console.log(error.response.data);
+                  console.log(error.response.status);
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  // The request was made but no response was received
+                  // `error.request` is an instance of XMLHttpRequest in the browser
+                  // and an instance of http.ClientRequest in node.js
+                  console.log(error.request);
+                } else {
+                  // Something happened in setting up the request that triggered an Error
+                  console.log('Error', error.message);
+                }
+              });
     }
 
     function updateLoad(){
@@ -208,6 +227,14 @@ export default function Page() {
             </ButtonGroup>
 
         </ContentBlock>
+        { loadError && (<ContentBlock>
+            <H>Произошла ошибка, попробуйте обновить данные</H>
+            <ButtonGroup>
+                
+                    <Button disabled={upRows} onClick={()=>(updateLoad())} secondary>{upRows ? "Обновление..." : "Обновить данные"}</Button>
+                
+            </ButtonGroup>
+        </ContentBlock>) }
 
         <TraectoryInfo loading={loading} rows={rows} />
         <UserInfo findUser={scrollTo} loading={loading} rows={rows} snils={data.snils} />
